96bd1b2728cdfd1b1a5e1c4b63d65e4cd61202af
Sonne002 <men_bmx@hotmail.com>
2022-11-27 17:33:28 -0300

    sirve

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = hola;
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = wenas;
+const nuevoNum = dios;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = funciono;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
